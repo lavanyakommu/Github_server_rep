f7a8b4bedb6184aabb78350d1176902368df8075
lavanyakommu <lkommu44@gmail.com>
2015-05-26 12:12:19 -0700

    second

---
+++ Jenkins_P1/src/test/java/P5/HomePage.java
@@ -0,0 +1,28 @@
+package P5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class HomePage 
+{
+
+	        WebDriver driver;
+	        public   HomePage(WebDriver driver)
+	        {
+	             this.driver=driver;
+	        }
+	        public void openUrl()
+	        {
+	                 driver.get("http://www.flipkart.com/");
+	                 driver.manage().window().maximize();
+	        }
+	        public void selectProduct()
+	        {
+	             
+	        	new Actions(driver).moveToElement(driver.findElement(By.xpath("//*[text()='Electronics']"))).perform();
+	        	driver.findElement(By.linkText("HP")).click();
+	        	
+	        }
+	}
+
